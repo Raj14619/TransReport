a803efc72a33f550be63802ca47487a41e89b0f6
Raj <33152218+Raj14619@users.noreply.github.com>
2022-02-15 23:13:27 +0000

    Finished test 1
    Test 1 complete

--- firstTest.js
+++ firstTest.js
@@ -42,23 +42,22 @@ async function loginAndPasswordFailureAssertion(){
 
     //
     
-    console.log("ok");
+ //   console.log("ok");
 
     var username = driver.findElement(By.id("user-name")).getText();
     
-    await driver.findElement(By.id("user-name")).sendKeys("standard_user");   
+    await driver.findElement(By.id("user-name")).sendKeys("12333");   
 
-    await driver.findElement(By.id("password")).sendKeys("secret_sauce", Key.ENTER);   
+    await driver.findElement(By.id("password")).sendKeys("12333", Key.ENTER);   
 
-    console.log(username);
+    //console.log(username);
 
-    //assert.strictEqual(username,"standard_user");
-         
-    var title = await driver.getElement(By.id(""));
-    console.log(title);
+     
+    var isLoginPasswordErrorMessageDisplayed = await driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[3]/h3")).isEnabled();
+    console.log(isLoginPasswordErrorMessageDisplayed);
 
-    assert.equal(title,"https://www.saucedemo.com/inventory.html");
-
+    assert.equal(isLoginPasswordErrorMessageDisplayed,"true");
 }
 
-loginAndPasswordSuccessAssertion();
+//loginAndPasswordSuccessAssertion();
+loginAndPasswordFailureAssertion();
