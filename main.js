07035a85dada2bf69bd1e3b18299b7d5aaec538d
Raj <33152218+Raj14619@users.noreply.github.com>
2022-02-15 18:08:12 +0000

    Project Setup
    Project is being setup - Able to navigate to the saucedemo website

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
 async function example() {
     let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
+    await driver.get("https://www.saucedemo.com/");
+  
+
 }
 example(); 
 
